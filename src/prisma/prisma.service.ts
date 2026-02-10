d95d982a945c5b53459b6d79b47b6116f6f43d43
Ray Santos <raysantosinsta@gmail.com>
2026-02-10 15:52:04 -0300

    removido redundancia e melhorar performance

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-this-alias */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
